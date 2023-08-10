63457ff8cc3200c42a05bd648cf63bd299fb6b75
AMG1995 <amjed1996@gmail.com>
2023-08-10 18:16:17 +0000

    hi

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main() {
-    puts("with proper grammar, but the outcome is a piece of art,\n"); // Outputs "Hello, World!" followed by a newline
-    return 0;
+/**
+ * main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
