ef64e870962f416d0240f8de2e24d4cda7c44c71
AMG1995 <amjed1996@gmail.com>
2023-08-10 17:49:55 +0000

    hi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    puts("Programming is like building a multilingual puzzle"); // Outputs "Hello, World!" followed by a newline
+    puts("\n"Programming is like building a multilingual puzzle"); /*Outputs "Hello, World!" followed by a newline*/
     return 0;
 }
