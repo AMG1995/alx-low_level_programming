65ab77adb537b213f3ebfc94945e19bd01c87823
AMG1995 <amjed1996@gmail.com>
2023-08-10 18:17:49 +0000

    hi

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - Prints out the sizes of various data types on the computer
+ * this program is compiled on.
+ * Return: 0 if success.
+ */
 int main(void)
 {
 	char c;
@@ -15,4 +19,3 @@ int main(void)
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
-
