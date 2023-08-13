64ac2c418040b82565ed052c73ce06a8074c4dd8
AMG1995 <amjed1996@gmail.com>
2023-08-13 13:09:32 +0000

    hi

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,35 +1,38 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
- * main - Determine if a random number is.
+ * main - print numbers from 00 to 99.
 (*
  * Return: 0 on success
  */
 int main(void)
 {
 	int i = '0';
-    int j = '0';
+	int j = '0';
 
-    for (i = '0' ; i < '9'; i++)
-    {
-        for (j = '1'; j <= '9'; j++)
-        {
-            /* code */
-           if (j>i)
-            {
-                /* code */
-                putchar(i);
-                putchar(j);
-                putchar(' ');
-                if (i != '8' && j != '9')
-                {
-                   putchar(',');
-                }
-
-            }
-
-        }
-    }
-    return (0);
+	while (i <= '9')
+	{
+		while (j <= '9')
+		{
+			if (!(i > j || i == j))
+			{
+				putchar(i);
+				putchar(j);
+				if (i == '8' && j == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
+		j = '0';
+		i++;
+	}
+	return (0);
 }
+
+
