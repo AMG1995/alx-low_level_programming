763baad35635ce5315047718e1cfe22210dd25c8
AMG1995 <amjed1996@gmail.com>
2023-08-13 11:52:52 +0000

    hi

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,10 +9,10 @@
 int main(void)
 {
     int i = '0';
-    int j = '0';
+    int j = '1';
     for (i = '0' ; i < '9'; i++)
     {
-        for (j = '0'; j <= '9'; j++)
+        for (j = '1'; j <= '9'; j++)
         {
             /* code */
             putchar(i);
