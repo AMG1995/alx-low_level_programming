84f6f1aba89068612395773af66565bbb68c513a
AMG1995 <amjed1996@gmail.com>
2023-08-15 20:03:14 +0000

    hi

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - Decide whether an integer is positive, negative or zero.
+ * @n: the number to check
+ * Return: 1 if positive or zero or minus
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('-');
+		return ('/');
+	}
+}
