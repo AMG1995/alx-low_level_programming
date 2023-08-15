f823a607fe0dc6b0c7e599c5d7a3d1e6d5cdbbcc
AMG1995 <amjed1996@gmail.com>
2023-08-15 13:36:13 +0000

    hi

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the alphabet in lowercase.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+
+}
