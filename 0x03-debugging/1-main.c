3c29b343b4bddfb58228e087c7c6935df64d06ac
AMG1995 <amjed1996@gmail.com>
2023-08-17 10:08:15 +0000

    hi

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 /*
-* while (i < 10)- it is comment
-* {
-* putchar(i)
-* }
+*	while (i < 10)- it is comment
+*	{
+*	putchar(i)
+*	}
 */
 	printf("Infinite loop avoided! \\o/\n");
 
