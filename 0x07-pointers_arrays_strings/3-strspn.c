08cf38b724786079bf6ac7ed7d9e9164cd08c136
AMG1995 <amjed1996@gmail.com>
2023-08-29 10:35:17 +0000

    hi

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+/**
+ * _strspn - gets the length of substring
+ * @s: segment to compare bytes
+ * @accept: string of bytes to compare
+ * Return: number of bytes in segment
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0' && s[i] != accept[j])
+			j++;
+		if (accept[j] == '\0')
+			return (i);
+		i++;
+	}
+	return (i);
+}
