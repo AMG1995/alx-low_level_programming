83411348f2c171f069f80e06758cf9aab66c6b4c
AMG1995 <39673621+AMG1995@users.noreply.github.com>
2023-09-27 00:16:25 +0400

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,51 +1,21 @@
 #include "lists.h"
 
 /**
- * print_listint - print the list.
- * @h: list.
- * Return: return the elements that linked.
+ * print_listint - prints all the elements of a linked list
+ * @h: head of the list
+ *
+ * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-int i = 0;
+	const listint_t *cursor = h;
+	size_t count = 0;
 
-	while (h != NULL)
+	while (cursor != NULL)
 	{
-	printf("%i\n", h->n);
-			h = h->next;
-			i++;
+		printf("%d\n", cursor->n);
+		count += 1;
+		cursor = cursor->next;
 	}
-	return (i);
-}
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
-
-/**
- * main - check the code for School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	listint_t *head;
-	listint_t *new;
-	listint_t hello = {8, NULL};
-	size_t n;
-
-	head = &hello;
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	new->n = 9;
-	new->next = head;
-	head = new;
-	n = print_listint(head);
-	printf("-> %lu elements\n", n);
-	free(new);
-	return (0);
+	return (count);
 }
