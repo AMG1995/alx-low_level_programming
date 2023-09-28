50022a9e1f6c5a960c608f5ef31879f06a366f05
AMG1995 <39673621+AMG1995@users.noreply.github.com>
2023-09-28 20:46:09 +0400

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,7 +5,7 @@
  * @n: A pointer to the unsigned long integer.
  * @index: The index of the bit to set (0-based).
  *
- * Return: 1 if it worked, or -1 if an error occurred (if index is out of range).
+ * Return: 1 if it worked, or -1 if an error occurred.
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
