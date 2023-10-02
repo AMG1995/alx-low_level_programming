658125fb484a7577f3f182b873fb23fc65db6934
AMG1995 <39673621+AMG1995@users.noreply.github.com>
2023-10-02 20:28:06 +0400

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,39 +1,38 @@
-#include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
+/*
+ * File: 1-create_file.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
 
 /**
- * create_file - Creates a file and writes text content to it.
- * @filename: The name of the file to create.
- * @text_content: The text content to write to the file.
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: 1 on success, -1 on failure.
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, bytes_written;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	/* Create the file or truncate it if it already exists */
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
-
 	if (text_content != NULL)
 	{
-		/* Write the text content to the file */
-		bytes_written = write(fd, text_content, strlen(text_content));
-		if (bytes_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	/* Close the file and return success */
-	close(fd);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
 	return (1);
 }
