b2d78defe40c0d4aae8acd7a3a90c1088c462203
AMG1995 <amjed1996@gmail.com>
2023-10-02 16:21:47 +0000

    hi

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,46 +11,46 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t file_descriptor, bytes_read, bytes_written;
-    char *buffer;
+	ssize_t file_descriptor, bytes_read, bytes_written;
+	char *buffer;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+	return (0);
 
-    /* Allocate memory for the buffer */
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-        return (0);
+	/* Allocate memory for the buffer */
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	return (0);
 
-    /* Open the file */
-    file_descriptor = open(filename, O_RDONLY);
-    if (file_descriptor == -1)
-    {
-        free(buffer);
-        return (0);
-    }
+	/* Open the file */
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+	{
+	free(buffer);
+	return (0);
+	}
 
-    /* Read from the file */
-    bytes_read = read(file_descriptor, buffer, letters);
-    if (bytes_read == -1)
-    {
-        free(buffer);
-        close(file_descriptor);
-        return (0);
-    }
+	/* Read from the file */
+	bytes_read = read(file_descriptor, buffer, letters);
+	if (bytes_read == -1)
+	{
+	free(buffer);
+	close(file_descriptor);
+	return (0);
+	}
 
-    /* Write to stdout */
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read)
-    {
-        free(buffer);
-        close(file_descriptor);
-        return (0);
-    }
+	/* Write to stdout */
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+	free(buffer);
+	close(file_descriptor);
+	return (0);
+	}
 
-    /* Clean up and close the file */
-    free(buffer);
-    close(file_descriptor);
+	/* Clean up and close the file */
+	free(buffer);
+	close(file_descriptor);
 
-    return (bytes_written);
+	return (bytes_written);
 }
